e3d85b05358132ed47030ca3dd5f3111d14d12d5
Lautaro <neulautaroramon@colegiomanoamiga.edu.ar>
2026-06-08 12:43:57 -0300

    first commit

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCFHO5xZCI6vCPx7W7v5HeerRENdtaK4Mw",
+  authDomain: "turnosya-cd3db.firebaseapp.com",
+  projectId: "turnosya-cd3db",
+  storageBucket: "turnosya-cd3db.firebasestorage.app",
+  messagingSenderId: "386497150988",
+  appId: "1:386497150988:web:e1341acc85291d082aae29",
+  measurementId: "G-GXCKT0E1VZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
